3561c1448c48e4d1e575e64a2c1d844545a80f97
Andre Adikara <andrepith@yahoo.co.uk>
2021-03-10 08:45:37 +0700

    Add react modal

--- src/containers/Contactify/index.js
+++ src/containers/Contactify/index.js
@@ -1,11 +1,16 @@
-import { useEffect, useCallback } from "react";
+import { useState, useEffect, useCallback } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import Modal from "react-modal";
 import { getListContact } from "../../store/actions";
 import Card from "../../components/Card";
 
+import { customStyles } from "./properties";
+
 const Contactify = () => {
+  const [showModal, setShowModal] = useState(false);
   const dispatch = useDispatch();
   const { contactList } = useSelector((state) => state);
+  const closeModal = () => setShowModal(false);
   const fetchApi = useCallback(() => {
     try {
       dispatch(getListContact());
@@ -14,7 +19,9 @@ const Contactify = () => {
     }
   }, [dispatch]);
 
-  const handleEdit = (contact) => () => {};
+  const handleEdit = (contact) => () => {
+    setShowModal(true);
+  };
 
   const handleRemove = (contact) => () => {};
 
@@ -23,20 +30,31 @@ const Contactify = () => {
   }, [fetchApi]);
 
   return (
-    <div>
-      <h1>Contact Management</h1>
-      <div className="list-items p-4">
-        {contactList?.map((contact, key) => (
-          <Card
-            contact={contact}
-            key={key}
-            handleEdit={handleEdit}
-            handleRemove={handleRemove}
-          />
-        ))}
+    <>
+      <div>
+        <h1>Contact Management</h1>
+        <div className="list-items p-4">
+          {contactList?.map((contact, key) => (
+            <Card
+              contact={contact}
+              key={key}
+              handleEdit={handleEdit}
+              handleRemove={handleRemove}
+            />
+          ))}
+        </div>
       </div>
-    </div>
+      <Modal
+        isOpen={showModal}
+        onRequestClose={closeModal}
+        style={customStyles}
+      >
+        <div>Henlo</div>
+      </Modal>
+    </>
   );
 };
+
+Modal.setAppElement("#root");
 
 export default Contactify;
