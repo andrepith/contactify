96d08e2339109fad7e08d190c6f67870e3b937ee
Andre Adikara <andrepith@yahoo.co.uk>
2021-03-10 08:30:12 +0700

    Create list with card component

--- src/containers/Contactify/index.js
+++ src/containers/Contactify/index.js
@@ -1,7 +1,40 @@
+import { useEffect, useCallback } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getListContact } from "../../store/actions";
+import Card from "../../components/Card";
+
 const Contactify = () => {
+  const dispatch = useDispatch();
+  const { contactList } = useSelector((state) => state);
+  const fetchApi = useCallback(() => {
+    try {
+      dispatch(getListContact());
+    } catch (error) {
+      console.error(error);
+    }
+  }, [dispatch]);
+
+  const handleEdit = (contact) => () => {};
+
+  const handleRemove = (contact) => () => {};
+
+  useEffect(() => {
+    fetchApi();
+  }, [fetchApi]);
+
   return (
     <div>
       <h1>Contact Management</h1>
+      <div className="list-items p-4">
+        {contactList?.map((contact, key) => (
+          <Card
+            contact={contact}
+            key={key}
+            handleEdit={handleEdit}
+            handleRemove={handleRemove}
+          />
+        ))}
+      </div>
     </div>
   );
 };
